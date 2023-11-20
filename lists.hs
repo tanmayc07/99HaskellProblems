c6f2fd007218c046c7c6aade13575add303ce6b8
Tanmay Chaudhari <tanmay@Tanmays-MacBook-Air.local>
2023-11-20 10:18:46 -0500

    :beers: Add list problem 2 solution

--- lists.hs
+++ lists.hs
@@ -4,3 +4,11 @@ myLast [] = error "Empty List Passed!"
 myLast (x:xs)
  | null xs = x
  | otherwise = myLast xs
+
+-- Q2: Find the last-but-one (or second-last) element of a list
+myButLast :: [a] -> a
+myButLast [] = error "Empty List Passed!"
+myButLast [x] = error "Only One Element in the List Passed!"
+myButLast (x:xs)
+ | length xs == 1 = x
+ | otherwise = myButLast xs
